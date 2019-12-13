c3b549782f861152ddbf8d0e401b4bcd51d42e46
moodyjmz <james.manuel@gmail.com>
2019-12-13 00:07:11 +0000

    Add material packages and ngx-date-fns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { DateFnsModule } from 'ngx-date-fns';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, DateFnsModule.forRoot(), BrowserAnimationsModule],
   providers: [
-    StatusBar,
-    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { DateFnsModule } from 'ngx-date-fns';
 
 import { HomePage } from './home.page';
-import {HeaderComponent} from '../../components/header/header.component';
-import {TeamComponent} from '../../components/team/team.component';
+import { TeamComponent } from '../../components/team/team.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { TeamMemberComponent } from '../../components/team-member/team-member.component';
 
 @NgModule({
   imports: [
@@ -18,8 +20,10 @@ import {TeamComponent} from '../../components/team/team.component';
         path: '',
         component: HomePage
       }
-    ])
+    ]),
+    DateFnsModule
   ],
-  declarations: [HomePage, HeaderComponent, TeamComponent]
+  declarations: [HomePage, TeamComponent, HeaderComponent, TeamMemberComponent]
 })
-export class HomePageModule {}
+export class HomePageModule {
+}
