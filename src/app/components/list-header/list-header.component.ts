3fee897a5c5f06c720045b5e385da1db1540a7a3
moodyjmz <james.manuel@gmail.com>
2019-12-15 16:24:56 +0000

    Add list header component :sparkles:

---
+++ src/app/components/list-header/list-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-header',
+  templateUrl: './list-header.component.html',
+  styleUrls: ['./list-header.component.scss'],
+})
+export class ListHeaderComponent implements OnInit {
+  @Input() text: string;
+  constructor() { }
+
+  ngOnInit() {}
+
+}
