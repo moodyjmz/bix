b24dc6554da1bf27acf82d989623bb4b68f5fcda
moodyjmz <james.manuel@gmail.com>
2019-12-16 00:04:01 +0000

    Add doc to dateUtils :pencil:

--- src/app/class/date-utils.ts
+++ src/app/class/date-utils.ts
@@ -1,12 +1,16 @@
 export class DateUtils {
     /**
-     * Return date object matching hours ago param
+     * @description Return date object matching hours ago param
      */
     static getHoursAgo(hours: number) {
         const d = new Date();
         d.setDate(d.getHours() - hours);
         return d;
     }
+
+    /**
+     * @description Generate a date before now, accepts optional ms as constraint
+     */
     static generateDateBeforeNow(ms?: number) {
         ms = !ms ? 86400000 : ms;
         const now = new Date();
