55a893122d625d4292d2cf3cae63a207f185f287
moodyjmz <james.manuel@gmail.com>
2019-12-21 00:08:43 +0000

    Style alert-item-component

--- src/app/components/alert-item/alert-item.component.ts
+++ src/app/components/alert-item/alert-item.component.ts
@@ -1,16 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Alert } from '../../models/alert/alert';
 
 @Component({
   selector: 'app-alert-item',
   templateUrl: './alert-item.component.html',
-  styleUrls: ['./alert-item.component.scss'],
+  styleUrls: ['./alert-item.component.scss']
 })
-export class AlertItemComponent implements OnInit {
+export class AlertItemComponent implements OnInit, OnChanges {
+  messageColor: string;
   @Input() alert: Alert;
   @Input() now: Date;
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  private setMessageColor() {
+    // can be hooked into a map/localised colour handling
+    this.messageColor =  this.alert.warning ? 'danger' : '';
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.setMessageColor();
+  }
 
 }
