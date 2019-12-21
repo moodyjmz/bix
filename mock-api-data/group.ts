42bbf6220c759c33eba6c271d6beab8e046a3496
moodyjmz <james.manuel@gmail.com>
2019-12-21 00:06:33 +0000

    Add mock group api data

--- mock-api-data/group.ts
+++ mock-api-data/group.ts
@@ -1,4 +1,7 @@
-export const GroupMeta = {
+export const MockData = {
   animalCount: 1345,
-  animalType: 'pig'
+  animalType: 'pig',
+  animalTypePlural: 'pigs',
+  id: 20190214,
+  started: '2018-09-01T00:00:00.000Z'
 };
